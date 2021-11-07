bf6ea74a5746220dec06ea3fc5fdebf364effe14
Magendra-ssn <85786067+Magendra-ssn@users.noreply.github.com>
2021-11-07 09:45:25 -0800

    Delete action.py

--- action.py
+++
@@ -1,6 +0,0 @@
-# distance between 2 obstacles = a
-# width of obstacle = b
-b=float(input("width of obstacle : "))
-print("deviate by a distance : ",b+20," mm")
-a=float(input("Distance to collsison : "))
-print("deviate before : ",2*a," mm")
